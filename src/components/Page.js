508bb9a55c566a7b90089c7347173afc713049bd
Michal Saletra <michal.saletra@syzygy.pl>
2021-07-06 23:37:55 +0200

    some work done

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,5 +1,8 @@
 import './Page.scss'
+import './reset.scss'
 import profile from '../images/profile.jpg'
+// import reactLogo from '../images/reactLogo.png'
+import {technologies, learning} from '../data'
 const Page = () => {
     return (
         <main className="page">
@@ -32,7 +35,32 @@ const Page = () => {
             </div>
             <div className="content">
                 <div className="content-left">
-                    <span>Bacon ipsum dolor amet andouille jerky biltong pork loin turkey meatball pancetta kevin chislic. Ham hock meatloaf flank pig ham ground round short ribs doner bresaola kielbasa hamburger chuck rump strip steak andouille. Pork chop tri-tip drumstick chuck. Biltong tri-tip sausage sirloin. Cupim chicken ground round boudin meatball. Strip steak bresaola meatloaf alcatra short ribs</span>
+                    <div className="column__section">
+                        <h2 className="column__section__title">Technologies I worked with:</h2>
+                        <ul className="column__section__list">
+                            {technologies.map(technology => (
+                                <li className="list-element">
+                                    <div className="technology">
+                                        <img src={technology.logo} className='technology__image' alt='profile img' />
+                                        <h3 className="technology__name">{technology.name}</h3>
+                                    </div>
+                            </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className="column__section">
+                        <h2 className="column__section__title">Technologies I am learning:</h2>
+                        <ul className="column__section__list">
+                            {learning.map(technology => (
+                                <li className="list-element">
+                                    <div className="technology">
+                                        <img src={technology.logo} className='technology__image' alt='profile img' />
+                                        <h3 className="technology__name">{technology.name}</h3>
+                                    </div>
+                            </li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
                 <div className="content-right">
                     <div className="section">
@@ -40,19 +68,42 @@ const Page = () => {
                             <img src={profile} className='section__image' alt='profile img' />
                             <h3 className="section__title">Profile</h3>
                         </div>
-                        <p className='section__content'>
+                        <div className="section__content">
+                        <p className='profile'>
                             Frontend developer with almost two years of experience in web development. During
                             last two years I found fullfi lment and joy while learing new technologies and frameworks. My experiences taught me that good frontend developer never stops learing and
                             seeks ways of self improvement. The most challenging project so far in whitch I took part
                             was a web applicaƟ on for IKEA Family (ikeafamily.eu). It experienced me in technologies
                             such as ReactJS, MobX, TypeScript, SASS and many more. Furthermore I learned how to
 look for tools that can provide soluƟ ons for various problems encountered during commercial web development. I aspire to become a ReactJS developer, but any commercial experience with JavaScript frameworks and libraries are precious to me. Currently I am planning to learn NodeJs and develop an applicaƟ on using both React and Node.</p>
+                        </div>
+                        
                     </div>
                     <div className="section">
                         <div className='section__header'>
                             <img src={profile} className='section__image' alt='profile img' />
-                            <h3 className="section__title">Profile</h3>
+                            <h3 className="section__title">Experience</h3>
+
                         </div>
+                    <div className="section__content">
+                    <div className="experience-element">
+                        <h3 className="experience-element__title">Any job you can think of</h3>
+                        <span className="experience-element__date">march 2017 - august 2018</span>
+                        <p className="experience-element__description">
+                            <span className="description-header">Responsibilities:</span>
+                            <span className="description-content">&nbsp;
+                                Preparing PSD based front-end, estimating tasks, maintaining web applications
+                            </span>
+                        </p>
+                        <p className="experience-element__description">
+                            <span className="description-header">Tooling:</span>
+                            <span className="description-content">&nbsp;
+                                Preparing PSD based front-end, estimating tasks, maintaining web applications
+                            </span>
+                        </p>
+                    </div>
+                    </div>
+                    
                         
             </div>
             <div className="section">
---
+++ src/data.js
@@ -0,0 +1,54 @@
+import reactLogo from './images/reactLogo.png'
+export const technologies = [
+  {
+    name: "ReactJS",
+    logo: reactLogo
+  },
+  {
+    name: "ReactJS",
+    logo: reactLogo
+  },
+  {
+    name: "ReactJS",
+    logo: reactLogo
+  },
+  {
+    name: "ReactJS",
+    logo: reactLogo
+  },
+  {
+    name: "ReactJS",
+    logo: reactLogo
+  },
+  {
+    name: "ReactJS",
+    logo: reactLogo
+  },
+  {
+    name: "ReactJS",
+    logo: reactLogo
+  },
+  {
+    name: "ReactJS",
+    logo: reactLogo
+  },
+  {
+    name: "ReactJS",
+    logo: reactLogo
+  },
+  {
+    name: "ReactJS",
+    logo: reactLogo
+  },
+];
+
+export const learning = [
+  {
+    name: "ReactJS",
+    logo: reactLogo
+  },
+  {
+    name: "ReactJS",
+    logo: reactLogo
+  }
+]
